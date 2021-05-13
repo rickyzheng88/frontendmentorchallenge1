8efa76ef3107554ae861b2ae325e7b13484ba7a0
Ricky Zheng <rickyzhenguny@gmail.com>
2021-05-13 19:43:57 -0400

    added ThirdContent component

--- src/components/ThirdContent/index.js
+++ src/components/ThirdContent/index.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 import Container from '../generics/Container';
+import TextContent from '../generics/TextContent';
+import backgroundImg from '../../images/illustration-laptop-desktop.svg';
 
 const Background = styled.div`
-    height: 1000px;
+    background-image: url(${backgroundImg});
+    background-repeat: no-repeat;
+    background-position: -16.25rem 6.5625rem;
+    background-size: 57.5rem;
+`;
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
 `;
 
 const ThirdContent = () => {
     return (
         <Background>
-            <Container paddingX='6.25rem'>
+            <Container paddingX='290px'>
+                <ContentWrapper>
+                    <TextContent.Wrapper width='500px'>
+                        <TextContent.Title>Free, open, Simple</TextContent.Title>
+                        <TextContent.Parraph paddingBottom='65px'>
+                            Blogr is a free open source application backed by a large
+                            community of helpful developers. It supports features such as code
+                            highlighting, Rss feeds, social media integration, third-party
+                            commenting tools, and works seamlessly with Google Analytics. The
+                            architecture is clean and is relatively ease to learn.
+                        </TextContent.Parraph>
+                    </TextContent.Wrapper>
 
+                    <TextContent.Wrapper width='500px'>
+                        <TextContent.Title>Powerful tooling</TextContent.Title>
+                        <TextContent.Parraph>
+                            Batteries included. We built a simple and straightfoward CLI tool that
+                            makes customization and deployment a breeze, but capable of
+                            producing even the most complicated sites.
+                        </TextContent.Parraph>
+                    </TextContent.Wrapper>
+                </ContentWrapper>
             </Container>
         </Background>
     );
